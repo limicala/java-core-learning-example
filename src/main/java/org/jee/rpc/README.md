dcb561f60b19d4cbd3c56fb4f38db039a0a952b7
renjie <2579749249@qq.com>
2017-07-08 19:49:58 +0800

    rpc

---
+++ src/main/java/org/jee/rpc/README.md
@@ -0,0 +1,13 @@
+一些疑问
+
+- ObjectInputStream
+- RPC、RMI、JMS
+
+RMI是远程方法调用，能够让在某个java虚拟机上的对象调用另一个java虚拟机的对象的方法。
+
+而RPC（Remote Procedure Call Protocol），远程过程调用协议，能实现跨语言调用
+我想了解的是这个
+
+[1](http://lavasoft.blog.51cto.com/62575/91679/)
+
+[深入浅出RPC](http://blog.csdn.net/mindfloating/article/details/39474123)
--- src/main/java/org/jee/rpc/RpcTest.java
+++ src/main/java/org/jee/rpc/RpcTest.java
@@ -25,6 +25,11 @@ public class RpcTest {
         RpcImporter<EchoService> importer = new RpcImporter<>();
 
         // 从服务本地代理获取服务对象类
+        // 这里importer的第一个参数是实现类的类，
+        // 因为这个例子本质上是让服务提供者通过动态代理产生一个实现类的对象
+        // 然后通过socket传递ObjectInputStream并转成对象
+        // 但是我理解的rpc是消费者只要知道接口类，服务者会返回这个接口的实现类
+        // 所以这个例子真正涉及到rpc的只是不多
         EchoService echo = importer.importer(EchoServiceImpl.class,new InetSocketAddress("localhost",8088));
         System.out.println(echo.echo("Are you OK?"));
     }
